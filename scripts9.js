a24c5607b36a4ae0498cd5e3157ce7f3f3444465
unknown <mrhdigital@yahoo.com>
2020-01-19 13:23:29 -0600

    work on logic to stop the setInterval function

--- scripts9.js
+++ scripts9.js
@@ -4,7 +4,11 @@ function timer(seconds) {
     const then = now + seconds * 1000;
     //console.log({now, then});
     setInterval(() => {
-        const secondsLeft = (then - Date.now()) / 1000;
+        const secondsLeft = Math.round((then - Date.now()) / 1000);
+        // check if we should stop it
+        If(secondsLeft <= 0) {
+            return; // return will not stop the function from running, it will not just display it
+        }
         console.log(secondsLeft);
     }, 1000);
 }
