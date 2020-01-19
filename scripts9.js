8ae67aa86c9b89146e796bb4e60c4fa7bca48035
unknown <mrhdigital@yahoo.com>
2020-01-19 13:49:14 -0600

    immediately run displayTimeLeft only once

--- scripts9.js
+++ scripts9.js
@@ -1,14 +1,23 @@
 //alert('Hi');
+let countdown;
+
 function timer(seconds) {
     const now = Date.now();
     const then = now + seconds * 1000;
+    displayTimerLeft(seconds); // immediately run this function only once
     //console.log({now, then});
-    setInterval(() => {
+    counstDown = setInterval(() => {
         const secondsLeft = Math.round((then - Date.now()) / 1000);
+
         // check if we should stop it
-        If(secondsLeft <= 0) {
+        if(secondsLeft < 0) {
+            clearInterval(counstDown);
             return; // return will not stop the function from running, it will not just display it
         }
-        console.log(secondsLeft);
+        displayTimerLeft(secondsLeft);
     }, 1000);
+}
+
+function displayTimerLeft(seconds) {
+    console.log(seconds);
 }
