d335d182492d5e8e547ce17d252a8b14ac0bd09f
unknown <mrhdigital@yahoo.com>
2020-01-19 13:18:46 -0600

    secondLeft displayed in console

--- scripts9.js
+++ scripts9.js
@@ -2,5 +2,9 @@
 function timer(seconds) {
     const now = Date.now();
     const then = now + seconds * 1000;
-    console.log({now, then});
+    //console.log({now, then});
+    setInterval(() => {
+        const secondsLeft = (then - Date.now()) / 1000;
+        console.log(secondsLeft);
+    }, 1000);
 }
